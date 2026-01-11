079c126975646512b8e473f978820c464a82ef70
Faiz <faizbot81@gmail.com>
2026-01-11 20:14:40 +0700

    update tampilan

--- src/views/Auth/Signin.vue
+++ src/views/Auth/Signin.vue
@@ -176,16 +176,16 @@ const loginHandler = async () => {
               <button
                 type="button"
                 @click="togglePasswordVisibility"
-                class="absolute right-3 top-1/2 -translate-y-1/2 text-gray-500 hover:text-blue-600"
+                class="absolute right-5 top-1/2 -translate-y-1/2 text-gray-500 hover:text-blue-600 "
               >
-                <svg v-if="!showPassword" class="w-5 h-5" fill="none" stroke="currentColor">
+                <svg v-if="!showPassword" class="w-6 h-6" fill="none" stroke="currentColor">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                     d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                     d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
                 </svg>
 
-                <svg v-else class="w-5 h-5" fill="none" stroke="currentColor">
+                <svg v-else class="w-6 h-6" fill="none" stroke="currentColor">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                     d="M3 3l18 18M9.88 9.88a3 3 0 014.24 4.24M13.88 18.88A10 10 0 0112 19c-4.48 0-8.27-2.94-9.54-7a9.97 9.97 0 011.56-3.03" />
                 </svg>
