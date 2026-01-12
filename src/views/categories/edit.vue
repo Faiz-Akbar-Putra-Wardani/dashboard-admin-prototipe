539787d71ee77706f9ee493d166690981c812319
Faiz <faizbot81@gmail.com>
2026-01-13 06:57:17 +0700

    update

--- src/views/categories/edit.vue
+++ src/views/categories/edit.vue
@@ -286,7 +286,7 @@ onMounted(fetchCategory);
                 d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
               ></path>
             </svg>
-            <span>{{ isSubmitting ? "Menyimpan..." : "Simpan Perubahan" }}</span>
+            <span>{{ isSubmitting ? "Menyimpan..." : "Perbarui Kategori" }}</span>
           </button>
         </div>
       </form>
