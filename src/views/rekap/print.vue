00d2fd2908ed3203b596b27a79ea7a87a56fd7c4
Faiz <faizbot81@gmail.com>
2026-01-13 22:26:29 +0700

    update lagi

--- src/views/rekap/print.vue
+++ src/views/rekap/print.vue
@@ -211,12 +211,12 @@ onMounted(() => {
         </svg>
         Download PDF
       </button>
-      <button @click="window.print()" class="btn btn-secondary ml-3">
+      <!-- <button @click="window.print()" class="btn btn-secondary ml-3">
         <svg class="w-5 h-5 inline-block mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 17h2a2 2 0 002-2v-4a2 2 0 00-2-2H5a2 2 0 00-2 2v4a2 2 0 002 2h2m2 4h6a2 2 0 002-2v-4a2 2 0 00-2-2H9a2 2 0 00-2 2v4a2 2 0 002 2zm8-12V5a2 2 0 00-2-2H9a2 2 0 00-2 2v4h10z" />
         </svg>
         Print
-      </button>
+      </button> -->
     </div>
 
     <!-- AREA PRINT -->
